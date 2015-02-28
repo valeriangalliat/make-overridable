d3783827c89b1f46365aa18df906731c87c2cea5
Valérian Galliat <val@codejam.info>
2015-02-28 11:54:19 +0100

    Remove unused helper

--- index.es6.js
+++ index.es6.js
@@ -1,7 +1,5 @@
 const extend = require('extend')
-
 const merge = (...args) => extend(true, {}, ...args)
-const clone = object => merge({}, object)
 
 const makeOverridable = f => object =>
   merge(f(object), {
