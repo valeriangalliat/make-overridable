2748ef62a52f8a5b91be425fd3de35ab33284a91
Valérian Galliat <val@codejam.info>
2015-02-28 12:43:15 +0100

    Clone before merging

--- index.es6.js
+++ index.es6.js
@@ -2,7 +2,7 @@ const extend = require('extend')
 const merge = (...args) => extend(true, {}, ...args)
 
 const makeOverridable = f => object =>
-  extend(f(object), {
+  extend(f(merge(object)), {
     override: newObject => makeOverridable(f)(merge(object, newObject))
   })
 
