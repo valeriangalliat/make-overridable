e5f08ae1ca50de41eec89ef900c2aaea0557996d
Valérian Galliat <val@codejam.info>
2015-02-28 12:36:42 +0100

    Remove useless clone

--- index.es6.js
+++ index.es6.js
@@ -2,7 +2,7 @@ const extend = require('extend')
 const merge = (...args) => extend(true, {}, ...args)
 
 const makeOverridable = f => object =>
-  merge(f(object), {
+  extend(f(object), {
     override: newObject => makeOverridable(f)(merge(object, newObject))
   })
 
